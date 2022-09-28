96cae019cfca4a5859ebe137bf5ac9e1cfce1cfe
Jillian Lehosky <jillianlehosky@gmail.com>
2022-09-27 20:12:42 -0400

    Ready to submit

--- Conversation.java
+++ Conversation.java
@@ -1,44 +1,93 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
+
 class Conversation {
 
   public static void main(String[] arguments) {
-    ArrayList<String> greetings = new ArrayList<String>();
-      greetings.add("Hello there! What is on your mind?");
-      greetings.add("Tell me about your day");
-
+//initialize arrays of generic closings and responses
     ArrayList<String> closings = new ArrayList<String>();
-      closings.add("Have a great day!");
-      closings.add("Goodbye!");
+      closings.add("Have a great day");
+      closings.add("Thanks for chatting");
+      closings.add("Have a slay day");
 
     ArrayList<String> responses = new ArrayList<String>();
       responses.add("Slay");
-      responses.add("Yasssss");
-      responses.add("Yeah, totally");
+      responses.add("Yas");
+      responses.add("Totally");
 
+//create scanner for user input
     Scanner input = new Scanner(System.in);
-    ArrayList <String> user_inputs = new ArrayList<String>();
-
-    //(can use same scanner - don't need new one every input from user)"""
-    System.out.println("Enter an integer: ");
+//create empty arrays to store user inputs and computer responses for printing transcript
+    ArrayList <String> user_input_store = new ArrayList<String>();
+    ArrayList <String> output_store = new ArrayList<String>();  
+//ask for number of rounds  
+    System.out.println("enter an integer: ");
     int numRounds = input.nextInt();
-    System.out.println("Hi! What's on your mind!");
+    input.nextLine(); //weird thing required to have proper number of rounds
+    System.out.println("Hi! What's on your mind? I am kind of stupid so no punctuation please");
       for (int i=0; i<numRounds; i++){
+        //get input from user
         String user_input = input.nextLine();
-        //that is what's prompting the users to respon
-        //adding to a list so we can print later
-        //this does it
-        user_inputs.add(user_input);
-        //this is for later step where we print out the list
-        System.out.println("Cool");
-      } input.close();
-      System.out.println("goodbye");
+        //add response to a list
+        user_input_store.add(user_input);
+       //run mirrorphrase command to mirror all aplicable words
+        String mirrorphrase = wordMirror(user_input);
+        if(mirrorphrase.equals(user_input)){ //if mirrorphrase didn't change any words
+          String response = getRandomElement(responses);//pick a random response
+          System.out.println(response);//display random response
+          output_store.add(response);//store for transcript printing
+        }
+        else { //if mirrorphrase changed any words
+          System.out.println(mirrorphrase + "?"); //print morrored sentence as a question
+          output_store.add(mirrorphrase + "?");//store for transcript printing
+        }   
+         }
+      input.close();//close scanner
+      String closing = getRandomElement(closings);//choose random closing
+      System.out.println(closing); //print closing
+      System.out.println(" ");//print empty line (not necessary, just looks nice)
+      System.out.println("Transcript:"); //print transcript header
+      for (int i=0; i<numRounds; i++){ //loop through number of rounds
+        System.out.println(user_input_store.get(i));//print user input for that round
+        System.out.println(output_store.get(i));//print output given for that round
+      }
+      System.out.println(closing);//print closing 
+    }
 
-    
-
-
-
-
-    // You will start the conversation here.
+  public static String wordMirror(String phrase){
+      String[] user_input_list = phrase.split(" "); // split string by spaces, turn into array
+      for (int i = 0; i < user_input_list.length; i++){ //iterate over each item in array
+        if (user_input_list[i].equals("i")){//if ith item equals a certain mirror word
+           user_input_list[i] = "you";}//replace it with complemantary mirror word
+        else if (user_input_list[i].equals("I")){
+          user_input_list[i]= "You";}
+        else if (user_input_list[i].equals("you")){
+          user_input_list[i]= "i";}
+        else if (user_input_list[i].equals("You")){
+            user_input_list[i]= "I";}
+        else if (user_input_list[i].equals("my")){
+          user_input_list[i] = "your";}
+        else if (user_input_list[i].equals("My")){
+            user_input_list[i] = "Your";}
+        else if (user_input_list[i].equals("your")){
+          user_input_list[i] = "my";}
+        else if (user_input_list[i].equals("Your")){
+          user_input_list[i] = "My";}
+        else if (user_input_list[i].equals("am")){
+          user_input_list[i] ="are";}
+        else if (user_input_list[i].equals("are")){
+          user_input_list[i] = "am";}
+          else if (user_input_list[i].equals("Are")){
+            user_input_list[i] = "Am";}
+        }
+        String mirroredphrase = String.join(" ",user_input_list); //make array a string again
+        return mirroredphrase;
+  }
+//code found from online to generate a random element out of an array
+  public static String getRandomElement(ArrayList<String> list)
+  {
+      Random rand = new Random();
+      return list.get(rand.nextInt(list.size()));
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,12 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+I worked with Dani Forte and talked to someone in class who'd name I totally forget and am very embarassed by that fact.
 - Which session(s) of TA / office hours did you attend?
+I was unable to attend any tutor or TA hours.
 - What are your initial impressions of Java? 
+I am starting to like it and understand it more after doing this assignment. Switching to object oriented is definitely very hard and I think my brain is still trying to do make that thinking switch. I'm still definitely stuggling with assigning variable types, and how calling a method affects the object it is being called on. 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+With nerly anything I wanted to do, there was already a built-in method or an "easy" way to accomplish a task. I tend to overcomplicate things when planning out a program and need to remind myself to take a step back and really look at all of the tools that the whatever language has accessible already. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+I think having a really detailed psuedocode plan for this assignment was super useful. I didn't really reference it once I started programming, but it helped me to realize everything I had to do. I also tried to get a minimally working program and added the more complicated elements in one at a time which made me feel secure in whatever I had done already. 
